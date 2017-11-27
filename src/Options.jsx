759f67b3d1b90aebc52cf1aff671b0eeb382650c
vic6 <vicmatthews116@gmail.com>
2017-11-26 18:39:32 -0800

    Add styling to options title bar

--- src/Options.jsx
+++ src/Options.jsx
@@ -4,12 +4,15 @@ import Option from "./Option.jsx";
 const Options = props => {
   return (
     <div>
-      <button
-        className="button button--link"
-        onClick={props.handleDeleteOptions}
-      >
-        Remove All
-      </button>
+      <div className='widget-header'>
+        <h3 className='widget-header__title'>Your Options</h3>
+        <button
+          className="button button--link"
+          onClick={props.handleDeleteOptions}
+        >
+          Remove All
+        </button>
+      </div>
       {props.options.length === 0 && <p>Enter an item to start</p>}
       {props.options.map(item => (
         <Option
