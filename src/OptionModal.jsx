67660d34c9496d287c230a4d49b2582bef5b3750
vic6 <vicmatthews116@gmail.com>
2017-11-27 19:12:25 -0800

    Style modal, add media queries

--- src/OptionModal.jsx
+++ src/OptionModal.jsx
@@ -6,10 +6,12 @@ const OptionModal = props => (
     isOpen={!!props.selectedOption}
     contentLabel="Selected Option"
     onRequestClose={props.clearModal}
+    closeTimeoutMS={200}
+    className="modal"
   >
-    <h3>Selected Option</h3>
-    <p>{props.selectedOption}</p>
-    <button onClick={props.clearModal}>Okay</button>
+    <h3 className='modal__title'>Selected Option</h3>
+    <p className='modal__body'>{props.selectedOption}</p>
+    <button className='button' onClick={props.clearModal}>Okay</button>
   </Modal>
 );
 
